479f48bd435136ce54230e1aefaa3d90f08a8306
JimSmit <38352013+JimSmit@users.noreply.github.com>
2020-12-05 17:40:48 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -175,4 +175,153 @@ def df_preparer(df,variables, val_share=0.25,test_share=0.2,random_state=0,norm=
     return df_train,df_val,df_test,df_demo_train,df_demo_val,df_demo_test
     
     
+
+def prepare_feature_vectors(df,df_train,df_demo,df_demo_train,df_patients,ids_IC_only,ids_events,pred_window,gap,int_neg,int_pos,feature_window,
+                        features,label_type='mortality'):
+    print('prepare_feature_vectors triggered')
+
+    """
+    Samples feature vectors from the input dfs. 
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        df to sample from.
+    df_train: pd.DataFrame
+        df containing training set. Imputed values are based on the training set. 
+    df_demo: pd.DataFrame
+        demograhics df to sample from.
+    df_demo_train: pd.DataFrame
+        demograhics df containing the training set. Imputed values are based on the training set. 
+    pred_window: int
+        Size of prediction window in hours
+    gap: int
+        Size of gap in hours
+    int_neg: int
+        Interval between samples for the negative class. In hours.
+    int_pos: int
+        Interval between samples for the positive class. In hours.
+    feature_window: int
+        Number of most recent assessments to be included in feature vector
+    variables: np.array[str]
+        Array of strings representing the names of the variables to be included in the model.
+
+
+    Returns
+    -------
+    X: matrix [N feature vectors x N variables]
+    y: vector [N feature vectors x 1]
+    type : np.array
+    """
+
+    from datetime import datetime, timedelta
+
+    pos = list() #create empty list for pos labeled feature vectors
+    neg = list() #create empty list for neg labeled feature vectors
+    
+    if label_type == 'mortality':
+        print('Label for mortality')
+        
+        df_pos = df[df['DEST'].contains('died')] 
+        df_neg = df[~df['DEST'].contains('died')]
+
+    elif label_type == 'ICU':
+        print('label for ICU admission')
+
+        df_pos = df[df['DEPARTMENT'].contains('ICU')] 
+        df_neg = df[~df['DEPARTMENT'].contains('ICU')]
+
+    print('pos df:',df_pos.shape, '-->',len(df_pos['PATIENTNR'].unique()), 'patients')
+    print('neg df:',df_neg.shape, '-->',len(df_neg['PATIENTNR'].unique()), 'patients')
+
+    print('-----Sampling for positive patients-----') 
+
+    count = 0 
+
+    for idx in np.unique(df_pos['ID']): # loop over patients
+        # print(idx)
+        patient = df_pos[df_pos['ID']==idx].sort_values(by='TIME').reset_index(drop=True) # Extract data of single patient, sort by date
+
+        if label_type == 'ICU':
+            t_event = patient[patient['DEPARTMENT']=='IC']['TIME'].min() # define moment of ICU admission as first ICU measurement
+        elif label_type == 'mortality':
+            t_event = patient['TIME'].max() 
+            
+            
+        if (t_event - patient['TIME'].min()).total_seconds()/3600 < gap: # cannot label patients for which time between start and event is shorter than the gap
+            count += 1
+        else:
+
+            #For positive feature vectors of positive patient
+            ts = []            
+            t = t_event - timedelta(hours=gap) #initialize t
+            window = pred_window
+
+            for i in range(int(window/int_pos)-1): # Make array with timestamps to sample from, making steps of size 'int_pos'
+                ts.append(t)
+                t = t - timedelta(hours=int_pos)
+
+            for t in ts: 
+
+                temp = patient[patient['TIME'] <= t].reset_index(drop=True)
+
+                v,med_share,ff_share,med_spec,ff_spec = create_feature_window(temp,df_train,df_demo,df_demo_train,feature_window,features,idx)
+                pos.append(v)
+
+            # For Negative feature vectors of positive patients
+            ts = []            
+            t = t_event - timedelta(hours=gap+pred_window) #initialize t 
+            window = (t_event - patient['TIME'].min()).total_seconds()/3600 - pred_window - gap # window to sample from is full window - prediction window and gap
+
+            for i in range(int(window/int_neg)-1): # Make array with timestamps to sample from, making steps of size 'int_neg'
+                ts.append(t)
+                t = t - timedelta(hours=int_neg)
+
+            for t in ts: 
+
+                temp = patient[patient['TIME'].dt.date <= t].reset_index(drop=True) # Extract snippet before this day
+
+                v,med_share,ff_share,med_spec,ff_spec = create_feature_window(temp,df_train,df_demo,df_demo_train,feature_window,features,idx)
+                neg.append(v)
+
+    print('-----Sampling for negative patient-----')
+
+    for idx in np.unique(df_neg['ID']): # loop over patients
+        # print(idx)
+        patient = df_neg[df_neg['ID']==idx].sort_values(by='TIME').reset_index(drop=True) # Extract data of single patient, sort by date
+
+        if (patient['TIME'].max() - patient['TIME'].min()).total_seconds()/3600 < gap: # cannot label patients with stay shorter than the gap
+            count+= 1
+
+        else:
+            ts = []            
+            t = patient['TIME'].max() #initialize t 
+            window = (patient['TIME'].max() - patient['TIME'].min()).total_seconds()/3600 # window to sample from is full window 
+
+            for i in range(int(window/int_neg)-1): # Make array with timestamps to sample from, making steps of size 'int_neg'
+                ts.append(t)
+                t = t - timedelta(hours=int_neg)
+
+            for t in ts: 
+
+                temp = patient[patient['TIME'].dt.date <= t].reset_index(drop=True) # Extract snippet before this day
+
+                v,med_share,ff_share,med_spec,ff_spec = create_feature_window(temp,df_train,df_demo,df_demo_train,feature_window,features,idx)
+                neg.append(v)
+
+    print('number of patients with too little data for feature vector: ', count)            
+
+    pos=np.array([np.array(x) for x in pos])
+    neg=np.array([np.array(x) for x in neg])
+    print('shape of positive class: ', pos.shape, 'shape of negative class: ', neg.shape)
+
+    X = np.concatenate((pos, neg), axis=0)
+    y = np.concatenate((np.ones(pos.shape[0]),np.zeros(neg.shape[0])),axis=0)
+
+    print(X.shape)
+    assert(np.isnan(X).any() == False)
+    print(y.shape)
+    assert(np.isnan(y).any() == False)
+
+    return X, y     
    
